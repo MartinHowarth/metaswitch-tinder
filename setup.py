1717c33f1ffe155ce4b2efd9e1d18d0d3caa932d
MartinHowarth <howarth.martin@gmail.com>
2019-03-07 19:07:44 +0000

    Resolve a downstream dependency clash

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         "plotly==2.6.0",
         "psycopg2",
         "requests==2.18.4",
-        # "requests_oauthlib==1.0.0",  # Not directly required, included to resolve a downstream dependency clash...
+        "requests_oauthlib==1.0.0",  # Not directly required, included to resolve a downstream dependency clash...
         "schematics",
         "sendgrid",
         "sqlalchemy-utils",
